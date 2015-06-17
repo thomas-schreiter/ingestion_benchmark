648f67705ab64a783f1d53b3ee979bd6e44a454e
Thomas Schreiter <thomas.schreiter81@gmail.com>
2015-06-17 03:46:42 +0000

    hello flask api python

---
+++ hello/flask_hello.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+    
